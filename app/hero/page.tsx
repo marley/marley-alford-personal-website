de8e988ed2505786e909befd1618087743837c41
Marley Alford <marleycodes@gmail.com>
2026-03-26 13:53:18 +0100

    feat: Update the overall styling

--- app/hero/page.tsx
+++ app/hero/page.tsx
@@ -9,28 +9,30 @@ export default function Hero() {
         since 2017.`;
   return (
     <div className="h-screen flex flex-col justify-between">
-      <div className="text-3xl">
-        Hi, my name is Marley. I have{" "}
-        <a href="https://www.linkedin.com/in/marleyalford/" target="_blank">
-          worked in tech
-        </a>{" "}
-        since{" "}
-        <ChainText
-          content="2017"
-          nextContent={`after an inspiring summer at <a href="https://hackny.org/" target="_blank">hackNY</a>`}
-        />
-        . Sometimes I also{" "}
-        <a
-          href="https://www.linkedin.com/posts/marleyalford_ive-been-teaching-intro-web-dev-with-frauenloop-share-7266847502950170625-ZIgN?utm_source=share&utm_medium=member_desktop&rcm=ACoAABgCsPMB_GB8ATVdsSDiUi0JSRA3BcMojQE"
-          target="_blank"
-        >
-          teach
-        </a>
-        . Currently building an inclusive and beneficial music ecosystem @{" "}
-        <a href="https://ohr-fm.vercel.app/" target="_blank">
-          ohr.fm
-        </a>
-        .{" "}
+      <div id="main" className="text-3xl">
+        <p>
+          Hi, my name is Marley. I have{" "}
+          <a href="https://www.linkedin.com/in/marleyalford/" target="_blank">
+            worked in tech
+          </a>{" "}
+          since{" "}
+          <ChainText
+            content="2017"
+            nextContent={`after an inspiring summer at <a href="https://hackny.org/" target="_blank">hackNY</a>`}
+          />
+          . Sometimes I also{" "}
+          <a
+            href="https://www.linkedin.com/posts/marleyalford_ive-been-teaching-intro-web-dev-with-frauenloop-share-7266847502950170625-ZIgN?utm_source=share&utm_medium=member_desktop&rcm=ACoAABgCsPMB_GB8ATVdsSDiUi0JSRA3BcMojQE"
+            target="_blank"
+          >
+            teach
+          </a>
+          . Currently building a more beneficial music ecosystem @{" "}
+          <a href="https://ohr-fm.vercel.app/" target="_blank">
+            ohr.fm
+          </a>
+          .{" "}
+        </p>
         <p>
           <ChainText
             content="Contact me"
