4eff60533cab111aa9de11aec5ef4f31cfb404e1
Marley Alford <marleycodes@gmail.com>
2026-03-27 14:13:46 +0100

    chore: Add github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/marley-alford-personal-website", // e.g. '/my-nextjs-app'
+  assetPrefix: "/marley-alford-personal-website/", // same value
 };
 
 export default nextConfig;
