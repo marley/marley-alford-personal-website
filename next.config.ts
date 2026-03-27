090c8a5a1ca5fad47c606bd35ecbe687c9901994
Marley Alford <marleycodes@gmail.com>
2026-03-27 19:16:56 +0100

    chore: remove github url to serve site at official url

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/marley-alford-personal-website", // e.g. '/my-nextjs-app'
-  assetPrefix: "/marley-alford-personal-website/", // same value
 };
 
 export default nextConfig;
